7a70ba32e92e61fc0983d8d982c40947405554ee
Azumabashi <hiro_kataoka@hkataoka.local>
2024-03-22 14:02:26 +0900

    test モジュールを使う

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 'use strict';
 const seventeen = require('./index.js');
 const assert = require('node:assert');
+const test = require('node:test');
 
-const a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60, 98, 38, 28, 96, 32];
-assert.deepStrictEqual(a.filter(seventeen.isMultipleOfSeventeen), [85, 34]);
+test('isMultipleOfSeventeenのテスト', () => {
+  const a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60, 98, 38, 28, 96, 32];
+  assert.deepStrictEqual(a.filter(seventeen.isMultipleOfSeventeen), [85, 34]);
+});
 
 console.log('テストが正常に完了しました');
